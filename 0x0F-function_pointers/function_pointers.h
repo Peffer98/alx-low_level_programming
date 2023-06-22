441adb948e037a292efa71334c6eba70b820b748
Peffer98 <dillonpeffer@gmail.com>
2023-06-22 16:53:26 -0700

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,7 @@
+#ifndef _HEADER_
+#define _HEADER_
+#include <stdlib.h>
+
 int _putchar(char c);
 
 void print_name(char *name, void (*f)(char *));
@@ -6,4 +10,4 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 
 int int_index(int *array, int size, int (*cmp)(int));
 
-
+#endif
